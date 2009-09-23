0e79a0fca6ad36ad50a63288b4e66b6f9178aa8f
Colby Gutierrez-Kraybill <colbygk@rubyforge.org>
2009-09-23 05:15:29 +0000

    Changes to allow for mapping of log4r levels to syslog ones, so that syslog API is not directly exposed

--- log4r/src/log4r/outputter/syslogoutputter.rb
+++ log4r/src/log4r/outputter/syslogoutputter.rb
@@ -12,25 +12,41 @@ require 'syslog'
 module Log4r
 
   # syslog needs to set the custom levels
-  if LNAMES.size > 1
-    raise LoadError, "Must let syslogger.rb define custom levels"
-  end
+  #if LNAMES.size > 1
+  #  raise LoadError, "Must let syslogger.rb define custom levels"
+  #end
   # tune log4r to syslog priorities
-  Configurator.custom_levels("DEBUG", "INFO", "NOTICE", "WARNING", "ERR", "CRIT", "ALERT", "EMERG")
+  #Configurator.custom_levels("DEBUG", "INFO", "NOTICE", "WARNING", "ERR", "CRIT", "ALERT", "EMERG")
+
+  SYSLOGNAMES = Hash.new
 
   class SyslogOutputter < Outputter
     include Syslog::Constants
 
-    # maps log4r levels to syslog priorities (logevents never see ALL and OFF)
-    SYSLOG_LEVELS = [
-        nil, 
-        LOG_DEBUG, 
-        LOG_INFO, 
-        LOG_WARNING, 
-        LOG_ERR, 
-        LOG_CRIT, 
-        nil
-    ]
+    # maps default log4r levels to syslog priorities (logevents never see ALL and OFF)
+    SYSLOG_LEVELS_MAP = {
+      "DEBUG"  => LOG_DEBUG, 
+      "INFO"   => LOG_INFO, 
+      "NOTICE" => LOG_NOTICE,  # by default NOTICE is not in log4r
+      "WARN"   => LOG_WARNING, 
+      "ERROR"  => LOG_ERR, 
+      "FATAL"  => LOG_CRIT,
+      "ALERT"  => LOG_ALERT,   # by default ALERT is not in log4r
+      "EMERG"  => LOG_EMERG,   # by default EMERG is not in log4r
+    }
+
+    SYSLOG_LOG4R_MAP = {
+      "DEBUG"  => "DEBUG", 
+      "INFO"   => "INFO", 
+      "WARN"   => "WARN", 
+      "ERROR"  => "ERROR", 
+      "FATAL"  => "FATAL"
+      # "NOTICE" => "INFO",      # by default NOTICE is not in log4r
+      # "ALERT"  => "FATAL",     # by default ALERT is not in log4r
+      # "EMERG"  => "FATAL"      # by default EMERG is not in log4r
+    }
+
+    @levels_map = SYSLOG_LOG4R_MAP
 
     # There are 3 hash arguments
     #
@@ -42,13 +58,14 @@ module Log4r
       ident = (hash[:ident] or hash['ident'] or _name)
       logopt = (hash[:logopt] or hash['logopt'] or LOG_PID | LOG_CONS).to_i
       facility = (hash[:facility] or hash['facility'] or LOG_USER).to_i
+      map_levels_by_name_to_syslog()
       @syslog = Syslog.open(ident, logopt, facility)
     end
 
     def closed?
       return !@syslog.opened?
     end
-    
+
     def close
       @syslog.close unless @syslog.nil?
       @level = OFF
@@ -56,19 +73,37 @@ module Log4r
       Logger.log_internal {"Outputter '#{@name}' closed Syslog and set to OFF"}
     end
 
+    # A single hash argument that maps custom names to syslog names
+    # 
+    # [<tt>levels_map</tt>]	A map that will create a linkage between levels
+    # 				in a hash and underlying syslog levels.
+    # 				By default, these are direct mapping of the log4r
+    # 				levels (e.g. "DEBUG" => "DEBUG")
+    # 				If you have defined your own custom levels, you
+    # 				should provide this underlying mapping, otherwise
+    # 				all messages will be mapped to the underlying syslog
+    # 				level of INFO by default.
+    def map_levels_by_name_to_syslog( lmap = SYSLOG_LOG4R_MAP )
+      @levels_map = lmap
+    end
+
+    def get_levels_map()
+      return @levels_map
+    end
+
     private
 
     def canonical_log(logevent)
-      pri = SYSLOG_LEVELS[logevent.level]
-      o = logevent.data
+      pri = SYSLOG_LEVELS_MAP[SYSLOG_LOG4R_MAP[LNAMES[logevent.level]]] rescue pri = LOG_INFO
+      o = format(logevent)
       if o.kind_of? Exception then
-        msg = "#{o.class} at (#{o.backtrace[0]}): #{o.message}"
+	msg = "#{o.class} at (#{o.backtrace[0]}): #{o.message}"
       elsif o.respond_to? :to_str then
-        msg = o.to_str
+	msg = o.to_str
       else
-        msg = o.inspect
+	msg = o.inspect
       end
-      
+
       @syslog.log(pri, '%s', msg)
     end
   end
