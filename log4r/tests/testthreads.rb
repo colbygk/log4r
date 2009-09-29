36233a92b5f11b317a35b4d2c1a32097913c2670
Colby Gutierrez-Kraybill <colbygk@rubyforge.org>
2009-09-29 17:56:35 +0000

    turn into actual TestCase class

--- log4r/tests/testthreads.rb
+++ log4r/tests/testthreads.rb
@@ -1,5 +1,5 @@
 # $Id$
-# Test sent in by chetreddy bug #27184
+# Test guts sent in by chetreddy bug #27184
 # 
 # Note: this test won't always catch a threading problem, as it
 # relies on a brute force approach.  NUM_THREADS can be increased
@@ -8,24 +8,16 @@
 # test.
 #
 
-$: << File.join("..","src")
-
-require 'rubygems'
-require 'log4r'
-
-NUM_THREADS=2000
-
-cnt = 0
-
-Log4r::Logger.new "Hello -1"
-
-begin
-  (0..NUM_THREADS).map do |i|
-    cnt = i
-    Thread.new do
-      Thread.current[:logger] = Log4r::Logger.new "Hello #{i}"
-    end
-  end.each { |thr| thr.join }
-rescue
-  puts "FAIL: #{$!} on iteration #{cnt}"
+class TestThreads < TestCase
+  def test_threads
+    NUM_THREADS=2000
+    assert_no_exception
+    {
+      (0..NUM_THREADS).map do |i|
+      Thread.new do
+	Thread.current[:logger] = Log4r::Logger.new "Hello #{i}"
+      end
+      end.each { |thr| thr.join }
+    }
+  end
 end
