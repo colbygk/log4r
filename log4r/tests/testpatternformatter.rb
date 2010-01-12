7f3e919a6f783bab171f82fa16fb0ba8e5609a8c
Colby Gutierrez-Kraybill <colbygk@rubyforge.org>
2010-01-12 09:28:07 +0000

    Tests for ndc,mdc,gdc

--- log4r/tests/testpatternformatter.rb
+++ log4r/tests/testpatternformatter.rb
@@ -1,10 +1,16 @@
-class TestPatternFormatter < TestCase
+$: << File.join("..","src")
+require "test/unit"
+require "log4r"
+include Log4r
+
+
+class TestPatternFormatter < Test::Unit::TestCase
   def test_pattern
     l = Logger.new 'test::this::that'
     l.trace = true
     o = StdoutOutputter.new 'test' 
     l.add o
-    assert_no_exception { 
+    assert_nothing_raised { 
     f = PatternFormatter.new :pattern=> "%d %6l [%C]%c %% %-40.30M"
                              #:date_pattern=> "%Y"
                              #:date_method => :usec
@@ -17,5 +23,56 @@ class TestPatternFormatter < TestCase
     l.fatal e
     l.info [1, 3, 5]
     }
+  end
+
+  def test_ndc
+    l = Logger.new 'test::this::that::other'
+    l.trace = true
+    o = StdoutOutputter.new 'testy' 
+    l.add o
+    f = PatternFormatter.new :pattern=> "%d %6l [%C]%c {%x} %% %-40.30M"
+                             #:date_pattern=> "%Y"
+                             #:date_method => :usec
+    Outputter['testy'].formatter = f
+
+    l.info "no NDC"
+    NDC.push("start")
+    l.info "start NDC"
+    NDC.push("finish")
+    l.info "start finish NDC"
+    NDC.pop()
+    l.info "start NDC"
+    NDC.remove()
+    l.info "no NDC"
+  end
+
+  def test_gdc
+    l = Logger.new 'test::this::that::other'
+    l.trace = true
+    o = StdoutOutputter.new 'testy' 
+    l.add o
+    f = PatternFormatter.new :pattern=> "%d %6l [%C]%c {%g} %% %-40.30M"
+                             #:date_pattern=> "%Y"
+                             #:date_method => :usec
+    Outputter['testy'].formatter = f
+
+    l.info "GDC default"
+    GDC.set("non-default")
+    l.info "GDC non-default"
+  end
+
+  def test_mdc
+    l = Logger.new 'test::this::that::other'
+    l.trace = true
+    o = StdoutOutputter.new 'testy' 
+    l.add o
+    f = PatternFormatter.new :pattern=> "%d %6l [%C]%c {%X{user}} %% %-40.30M"
+                             #:date_pattern=> "%Y"
+                             #:date_method => :usec
+    Outputter['testy'].formatter = f
+
+    l.info "no user"
+    MDC.put("user","colbygk")
+    l.info "user colbygk"
   end
 end
