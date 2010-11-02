03d22288b7a6df506ac4325de697bba68ec9ecfa
Mark Lewandowski <mark.e.lewandowski@gmail.com>
2010-11-02 15:21:25 -0700

    Default params, separated name and category
    * Added default values to host, port, and category in ScribeOutputter
    * Separated outputter name and scribe category

--- lib/log4r/outputter/scribeoutputter.rb
+++ lib/log4r/outputter/scribeoutputter.rb
@@ -7,20 +7,22 @@ require "scribe"
 
 module Log4r
   class ScribeOutputter < Outputter
-    attr_reader :host, :port
+    attr_reader :host, :port, :category
 
     def initialize(_name, hash={})
       super(_name, hash)
-      @host = hash[:host]
-      @port = hash[:port]
-      @client = Scribe.new("#{@host}:#{@port}", category=_name, add_newlines=false)
+      @host = (hash[:host] or hash[:host] or 'localhost')
+      @port = (hash[:port] or hash[:port] or '1463')
+      @category = (hash[:category] or hash[:category] or 'default')
+
+      @client = Scribe.new("#{@host}:#{@port}", category=@category, add_newlines=false)
     end
 
     private
 
     def write(data)
       begin
-        @client.log(data)
+        @client.log(data.strip, @category)
       rescue ScribeThrift::Client::TransportException => e
         Logger.log_internal(-2) {
           "Caught TransportException, is the scribe server alive?"
