4219339c4922bde0ac8988f906e9e45fedd2ff85
Mark Lewandowski <mark.e.lewandowski@gmail.com>
2010-11-02 11:02:42 -0700

    ScribeOutputter works with yaml configurator

--- lib/log4r/outputter/scribeoutputter.rb
+++ lib/log4r/outputter/scribeoutputter.rb
@@ -8,15 +8,17 @@ require "scribe"
 module Log4r
   class ScribeOutputter < Outputter
     attr_reader :host, :port
+
     def initialize(_name, hash={})
       super(_name, hash)
-      @client = Scribe.new("#{:host}:#{:port}", category=_name, add_newlines=false)
+      @host = hash[:host]
+      @port = hash[:port]
+      @client = Scribe.new("#{@host}:#{@port}", category=_name, add_newlines=false)
     end
 
     private
 
     def write(data)
-      puts "data=#{data}"
       @client.log(data.strip)
     end
   end
