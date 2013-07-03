f233f30eba3bfaf520e9204a6b67e6d894decc50
Dean Shelton <deanshelton913@gmail.com>
2013-07-03 15:14:19 -0700

    update rabbit outputter to take external yaml configurations

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source :gemcutter
-
+gem 'bunny', :git => "git://github.com/ruby-amqp/bunny.git", :branch => "master"
 gemspec
--- lib/log4r/outputter/rabbitoutputter.rb
+++ lib/log4r/outputter/rabbitoutputter.rb
@@ -1,26 +1,54 @@
 # :nodoc:
 require "rubygems"
+require 'logger'
 require "log4r/outputter/outputter"
 require 'bunny'
+require 'yaml'
 
 module Log4r
   # See log4r/logserver.rb
   class RabbitOutputter < Outputter
 
     def initialize(_name, hash={})
+      # Configuration defaults
       super(_name, hash)
-        conn = Bunny.new(user:'seanet', pass:'s34Ch1ck3n', vhost:'/seanet', host:'rabbit.mtnsatcloud.com')
-        conn.start
-
-        ch = conn.create_channel
-        @q  = ch.queue('seanet_logs', auto_delete: false, durable: true)
+      @path_to_yaml_file = "#{Rails.root}/config/rabbitmq.yml"
+      @config = { username: '', password: '', vhost: '', host: '', queue: '' }
+      if File.exist? @path_to_yaml_file
+        if @config = YAML::load(IO.read(@path_to_yaml_file))
+          @config.symbolize_keys!
+          stderr_log @config
+          @queue_name = @config.delete :queue
+          start_bunny
+        else
+          stderr_log "Malformed configuration file [#{@path_to_yaml_file}]"
+        end
+      else
+        stderr_log "Unable to find rabbit configuration file [#{@path_to_yaml_file}]"
+      end
     end
 
+    def start_bunny
+      begin
+        stderr_log "Starting Bunny Client"
+        conn = Bunny.new @config
+        conn.start
+        ch = conn.create_channel
+        @queue  = ch.queue(@queue_name, auto_delete: false, durable: true)
+      rescue Bunny::TCPConnectionFailed => e
+        stderr_log "rescued from: #{e}. Unable to connect to Rabbit Server"
+      end
+    end
+
+    def stderr_log(msg)
+      $stderr.puts "[#{Time.now.utc}] #{msg}"
+    end
+    
     private
 
-    def write(data)
-      @q.publish(data, :routing_key => @q.name)
-    end
+      def write(data)
+        @queue.publish data, { routing_key: @queue.name } unless @queue.nil?
+      end
+
   end
 end
-
