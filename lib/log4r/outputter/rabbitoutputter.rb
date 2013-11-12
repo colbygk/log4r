d3d0fddd440f55e986095fc5b86e24fd69bdcea9
Grady Griffin <gradygriffin@gmail.com>
2013-11-12 10:56:03 -0500

    refactoring configuration file load

--- lib/log4r/outputter/rabbitoutputter.rb
+++ lib/log4r/outputter/rabbitoutputter.rb
@@ -12,19 +12,11 @@ module Log4r
     def initialize(_name, hash={})
       # Configuration defaults
       super(_name, hash)
-      @config = {:host => 'localhost'}
-      if File.exist? @path_to_yaml_file
-        if settings = YAML::load(IO.read(@path_to_yaml_file))
-          @config = settings.include?(Rails.env) ? settings[Rails.env] : settings
-          @config.symbolize_keys!
-          @queue_name = @config.delete(:queue) || ''
-          start_bunny rescue nil
-        else
-          stderr_log "Malformed configuration file [#{@path_to_yaml_file}]"
-        end
-      else
-        stderr_log "Unable to find rabbit configuration file [#{@path_to_yaml_file}]"
-      end
+      stderr_log "Unable to find rabbit configuration file" unless @config
+      @config ||= {:host => "localhost"}
+      @config.symbolize_keys!
+      @queue_name = @config.delete(:queue) || ''
+      start_bunny rescue nil
     end
 
     def load_config_file(name)
@@ -33,23 +25,12 @@ module Log4r
       end
     end
 
-    def load_legacy_format()
-      file = YAML::load(IO.read(legacy_filename)) 
-    end
-
-    def load_new_format
-      file = YAML::load(IO.read(filename))
-    end
-
     def load_config
-      bunny = "bunny.yml"
-      rabbit = "rabbitmq.yml"
-      @config = load_config_file("bunny.yml") 
-      @config = 
-        load_config_file(bunny)[Rails.env]
-      elsif File.exist?("rabbitmq.yml")
-
-      return load_legacy_format if File.exist?(legacy_filename)
+      if load_config_file("bunny.yml")
+        @config = @config[Rails.env]
+      else
+        load_config_file("rabbitmq.yml")
+      end
     end
 
     def start_bunny
